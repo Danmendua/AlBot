184ce6d3e12008992f76b10249c663239083c727
Danmendua <danmendua@gmail.com>
2023-12-01 16:32:56 -0300

    update files

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -3,8 +3,8 @@ const knex = require('../connections/databaseConnection');
 const alredyExist = async (req, res, next) => {
     const { email } = req.body;
     try {
-        const encontrarUsuario = await knex('usuarios').where({ email }).first();
-        if (encontrarUsuario) {
+        const userFound = await knex('usuarios').where({ email }).first();
+        if (userFound) {
             return res.status(404).json({ mensagem: "Conta existente" });
         };
         next();
